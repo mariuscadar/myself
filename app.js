992405b2c72219193d5f477a254525d95fd15979
Marius Cadar <marius.cadar@gmail.com>
2017-12-13 23:43:37 +0200

    Serving index.html

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello world!")
+var http = require('http');
+var fs = require("fs");
+fs.readFile("index.html", function(err, data){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+  });
